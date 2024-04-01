1c5ff6ae4c90d76756046110b690cdf6977dd56c
NatalySolis <natycrotez1@outlook.es>
2024-04-01 11:02:45 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo de MVC2/Ejemplo de MVC2/Controllers/vistasRazorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_de_MVC2.Controllers
+{
+    public class vistasRazorController : Controller
+    {
+
+        // GET: vistasRazor
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult bloqueMultiple()
+        {
+            return View();
+        }
+    }
+}
